e7cc1192ac4bf3d10c3137e9e28c5b694bff30cf
Haafii <hafismuhammedtk@gmail.com>
2024-02-06 12:00:58 +0530

    voice recorded

--- src/screens/Speaking.jsx
+++ src/screens/Speaking.jsx
@@ -1,10 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { ObjectImages } from '../../assets/images/Objects/ObjectImages';
+import { Audio } from 'expo-av';
 
 const Speaking = () => {
   const [randomImage, setRandomImage] = useState(null);
   const [showMicrophone, setShowMicrophone] = useState(false);
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
+
+  async function startRecording() {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        const { recording } = await Audio.Recording.createAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+        setRecording(recording);
+      }
+    } catch (err) { }
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+    let allRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    allRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    setRecordings(allRecordings);
+
+  }
+
+  function getDurationFormatted(milliseconds) {
+    const minutes = milliseconds / 1000 / 60;
+    const seconds = Math.round((minutes - Math.floor(minutes)) * 60);
+    return seconds < 10 ? `${Math.floor(minutes)}:0${seconds}` : `${Math.floor(minutes)}:${seconds}`
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>
+            Recording #{index + 1} | {recordingLine.duration}
+          </Text>
+          <Button onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+        </View>
+      );
+    });
+  }
+
+  function clearRecordings() {
+    setRecordings([])
+  }
 
   useEffect(() => {
     const getRandomImage = () => {
@@ -32,7 +88,13 @@ const Speaking = () => {
       )}
       {showMicrophone && (
         <View style={styles.microphoneContainer}>
-          <Text style={styles.microphoneText}>🎤</Text>
+          <View style={styles.container}>
+            <TouchableOpacity onPress={recording ? stopRecording : startRecording} >
+              <Text className="text-3xl">{recording ? '🛑' : '🎤'}</Text>
+            </TouchableOpacity>
+            <Button title={recordings.length > 0 ? 'clear Recordings' : ''} onPress={clearRecordings} />
+            {getRecordingLines()}
+          </View>
         </View>
       )}
     </View>
@@ -48,9 +110,6 @@ const styles = StyleSheet.create({
   },
   microphoneContainer: {
     marginTop: 20,
-  },
-  microphoneText: {
-    fontSize: 30,
   },
 });
 
